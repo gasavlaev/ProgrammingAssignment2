98222eb2f119c00a8177c5ff4138f76755227f32
gasavlaev <56293927+gasavlaev@users.noreply.github.com>
2019-10-08 14:44:42 +0300

    Try #1000000000000050000000
    I hope I can upload this R file with all results. Otherwise I will also upload txt file with results and 1 screenshot file. Don't judge me, pls.

--- cachematrix.R
+++ cachematrix.R
@@ -5,11 +5,32 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
+i <- NULL
+        set <- function(y) {
+                x <<- y
+                i <<- NULL
+        }
+        get <- function() x
+        setInv <- function(ierse) i <<- ierse
+        getInv <- function() i
+        list(set = set,
+             get = get,
+             setInv = setInv,
+             getInv = getInv)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## Return a matrix that is the ierse of 'x'
+i <- x$getInv()
+        if (!is.null(i)) {
+                message("getting cached data")
+                return(i)
+        }
+        mat <- x$get()
+        i <- solve(mat, ...)
+        x$setInv(i)
+        i
 }
